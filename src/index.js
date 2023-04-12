bbf741d537ce52d58683ae7d5f775c799b493cdb
Ati <eskafiati@gmail.com>
2023-04-12 19:42:38 +1200

    TODOs removed

--- src/index.js
+++ src/index.js
@@ -238,11 +238,3 @@ celsius.addEventListener("click", updateToCelsius);
 
 // updating data while loading the homepage
 updateDataOnLoad();
-
-// TODO - update icon image
-// FIXME - remember to setAttribute for icon's alt
-
-// TODO - update time according to the API's timestamp
-
-// TODO - add next days forcast
-// FIXME - change the colors of numeric elements according to theme
